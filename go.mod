84acdedfa9725e313b91d83538e8300fcf0d6cf0
Andrew <me@radi8.dev>
2025-12-14 10:58:40 -0700

    initial commit of fastapkcache

---
+++ go.mod
@@ -0,0 +1,49 @@
+module github.com/radeeyate/fastapkcache
+
+go 1.25.1
+
+require (
+	github.com/charmbracelet/log v0.4.2
+	github.com/gofiber/fiber/v3 v3.0.0-rc.3
+	github.com/golobby/config/v3 v3.4.2
+)
+
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/colorprofile v0.2.3-0.20250311203215-f60798e515dc // indirect
+	github.com/charmbracelet/lipgloss v1.1.0 // indirect
+	github.com/charmbracelet/x/ansi v0.8.0 // indirect
+	github.com/charmbracelet/x/cellbuf v0.0.13-0.20250311204145-2c3ea96c31dd // indirect
+	github.com/charmbracelet/x/term v0.2.1 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/golobby/cast v1.3.3 // indirect
+	github.com/golobby/dotenv v1.3.2 // indirect
+	github.com/golobby/env/v2 v2.2.4 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/muesli/termenv v0.16.0 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+require (
+	github.com/andybalholm/brotli v1.2.0 // indirect
+	github.com/gofiber/schema v1.6.0 // indirect
+	github.com/gofiber/utils/v2 v2.0.0-rc.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/klauspost/compress v1.18.2 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/philhofer/fwd v1.2.0 // indirect
+	github.com/tinylib/msgp v1.5.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.68.0 // indirect
+	golang.org/x/crypto v0.44.0 // indirect
+	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/text v0.31.0 // indirect
+	resty.dev/v3 v3.0.0-beta.5
+)
---
+++ main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+
+	"github.com/charmbracelet/log"
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/logger"
+	"github.com/gofiber/fiber/v3/middleware/static"
+	"github.com/golobby/config/v3"
+	"github.com/golobby/config/v3/pkg/feeder"
+	"resty.dev/v3"
+)
+
+type Config struct {
+	Repositories []struct {
+		Identifier string // need to validate
+		BaseURL    string // need to validate
+	}
+}
+
+var activeDownloads sync.Map
+
+type RequestGroup struct {
+	wg   sync.WaitGroup
+	data []byte
+	err  error
+}
+
+func main() {
+	app := fiber.New()
+
+	client := resty.New()
+	defer client.Close()
+
+	repoConfig := Config{}
+	tomlFeeder := feeder.Toml{Path: "config.toml"}
+	c := config.New()
+	c.AddFeeder(tomlFeeder)
+	c.AddStruct(&repoConfig)
+	err := c.Feed()
+
+	if err != nil {
+		log.Fatal("Error parsing config", "err", err)
+	}
+
+	app.Use(logger.New(logger.Config{
+		Format:     "${time} - ${status} - ${ip} ${method} ${path}\n",
+		TimeFormat: "15:04:05 on 01/02/2006",
+		TimeZone:   "America/Denver",
+	}))
+
+	err = os.Mkdir("./repos", 0777)
+	if err != nil && !errors.Is(err, os.ErrExist) {
+		log.Fatal("Error creating repository directory", "err", err)
+	}
+
+	for _, repository := range repoConfig.Repositories {
+		repositoryBasePath := "./repos/" + repository.Identifier
+		err = os.Mkdir(repositoryBasePath, 0777)
+		if err != nil && !errors.Is(err, os.ErrExist) {
+			log.Fatal(
+				"Error creating repository directory",
+				"repo_id",
+				repository.Identifier,
+				"err",
+				err,
+			)
+		}
+
+		app.Use("/"+repository.Identifier, func(c fiber.Ctx) error {
+			trimmedPath := strings.TrimPrefix(c.Path(), "/"+repository.Identifier)
+			retrievalPath := repository.BaseURL + trimmedPath
+
+			if _, err := os.Stat(repositoryBasePath + trimmedPath); err == nil {
+				log.Info("Package in cache", "package", trimmedPath)
+				return c.Next()
+			}
+
+			if actual, loaded := activeDownloads.Load(retrievalPath); loaded &&
+				filepath.Base(trimmedPath) != "APKINDEX.tar.gz" {
+				reqGroup := actual.(*RequestGroup)
+				log.Info("Waiting for existing download", "package", trimmedPath)
+
+				reqGroup.wg.Wait()
+
+				if reqGroup.err != nil {
+					return c.SendStatus(fiber.StatusBadGateway)
+				}
+
+				return c.Next()
+			}
+
+			reqGroup := &RequestGroup{}
+			reqGroup.wg.Add(1)
+			activeDownloads.Store(retrievalPath, reqGroup)
+
+			defer func() {
+				activeDownloads.Delete(retrievalPath)
+				reqGroup.wg.Done()
+			}()
+
+			log.Info("Downloading Alpine Package", "package", trimmedPath)
+
+			res, err := client.R().Get(retrievalPath)
+			if err != nil {
+				log.Error("Failed to download", "package", trimmedPath, "error", err)
+				reqGroup.err = err
+				return c.SendStatus(fiber.StatusBadGateway)
+			}
+
+			if res.StatusCode() != 200 {
+				log.Error("Failed to download", "package", trimmedPath, "status", res.StatusCode())
+				return c.SendStatus(res.StatusCode())
+			}
+
+			// return APKINDEX.tar.gz from memory, do not cache to disk
+			if filepath.Base(trimmedPath) == "APKINDEX.tar.gz" {
+				c.Attachment("APKINDEX.tar.gz")
+				return c.SendStream(res.Body)
+			}
+
+			err = os.MkdirAll(repositoryBasePath+filepath.Dir(trimmedPath), 0777)
+			if err != nil {
+				reqGroup.err = err
+				log.Error(
+					"Error creating repository directories",
+					"path",
+					repositoryBasePath+filepath.Dir(trimmedPath),
+					"err",
+					err,
+				)
+
+			}
+
+			err = os.WriteFile(repositoryBasePath+trimmedPath, res.Bytes(), 0644)
+			if err != nil {
+				reqGroup.err = err
+				log.Error(
+					"Error writing package",
+					"path",
+					repositoryBasePath+trimmedPath,
+					"err",
+					err,
+				)
+			}
+
+			return c.Next()
+		}, static.New(repositoryBasePath))
+	}
+
+	err = app.Listen(":3000")
+	if err != nil {
+		log.Fatal("Failed to start server", "err", err)
+	}
+}
